665966934577e270537c2b3d888b27bde09b179f
PythonWork <id-baicai@qq.com>
2017-08-11 00:36:38 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-service bt start && cat \
-&& echo  "Bt-Panel: http://$address:$port" \
-&& echo -e "username: $username" \
-&& echo -e "password: $password"
-
-#cd /www/server/panel && python tools.pyc panel mkaliez.com
+cd /www/server/panel && python tools.pyc panel mkaliez.com
+service bt start && cat
