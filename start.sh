be03c122a71c1c1aace59556e112309b6de25295
PythonWork <id-baicai@qq.com>
2017-08-11 00:52:35 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cd /www/server/panel && python tools.pyc panel mkaliez.com
-service bt start && cat
+service bt start \
+&& cat \
+&& cd /www/server/panel \
+&& python tools.pyc panel mkaliez.com
